2885725eadf0c57a4e9fab0a0a73c6f72871d024
Adliano <adliano@me.com>
2019-03-20 12:02:59 -0700

    add the global variable (array) to hold images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,12 +10,18 @@ $(document).ready(function () {
     // 2)  to know witch on as select we can remove a special class name and get all the children using class name
     // 3)  load all the other img to second row and change it to bg-danger
 
+    // **** Global Variables **** \\
     // TODO: If no use remove
-    // Flags (Booleans) used to control when user selects the Attacker and defender
+    // Flags (Booleans) used to control 
+    //when user selects the Attacker and defender
     var isAttackerSelected = false;
     var isEnemySelected = false;
+    var attackerImages = [];
+    var enemyImages = [];
+    var attackerIndex = 0;
+    // ************************* //
 
-    ////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////// ONCLICK ////////////////////////////////////////
     //// onclick event added using class selector $(".setAttacker") for every image ////
     //// this will be the first event listener to be called, inside we will remove  ////
     //// the selector to void this listener be called until game is reload          ////
@@ -25,17 +31,21 @@ $(document).ready(function () {
         //console.log("clicked on image");
         //$(this).removeClass("bg-success");
 
-        // Get Array[] with attackers images
-        let topImages = $("#attackerImgsColumn").children();
-        // Get Array[] with enemy images
-        let bottonImages = $("#enemyImgsColumn").children();
+        // Get HTML Colection with attackers images
+        attackerImages = $("#attackerImgsColumn").children();
+
+        // Get HTML Colection with enemy images
+        enemyImages = $("#enemyImgsColumn").children();
+
         // Get the index of the clicked (Selected) character image
-        let attackerIndex = Array.from(topImages).indexOf(event.target);
+        // by change the HTML Collection to an Array[]
+        attackerIndex = Array.from(attackerImages).indexOf(event.target);
         // ------ debugging ----- //
         //console.log(attackerIndex);
 
         // using map to interate 
-        topImages.map((i, element) => {
+        attackerImages.map((i, element) => {
+            // get the current element on loop
             let currentCharacter = $(element);
 
             // remove this onclick event from img to void user click on it again
@@ -66,23 +76,43 @@ $(document).ready(function () {
                 // removing hidden-element class from the others characters on second row
                 // this will display enemy to be selected by user
                 // the second row event will be handle by another function below $(".setEnemy")...
-                $(bottonImages[i]).removeClass("hidden-element");
+                $(enemyImages[i]).removeClass("hidden-element");
                 //    add class to display then
-                //    $(bottonImages[i]).addClass("show");
+                //    $(enemyImages[i]).addClass("show");
                 //    add a class to make the next event be handle by the next event listener
-                //    $(bottonImages[i]).addClass("setEnemy");     
+                //    $(enemyImages[i]).addClass("setEnemy");     
             } 
         });
-
-    }); // :::: end of attackerImage event listener ::::
+    }); // :::: end of attackerImage onclick event listener ::::
 
     /////////////////////////////////////////////////////////////////////////
-    // enemyImage event listner, this event will handle the enyme selection 
-    // by user
+    //// onclick event for selectro $("".setEnemy") event listner,       ////
+    //// this event will handle the enemy selection by user              ////
     /////////////////////////////////////////////////////////////////////////
     $(".setEnemy").on("click", function (event) {
+        //
+        let enemyIndex = Array.from(enemyImages).indexOf(event.target);
+
         // ------ debugging ------- //
-        // console.log("inside enemyEvent");
+        console.log("inside enemyEvent");
+        console.log(`Enemy Index ${enemyIndex}`);
+
+        enemyImages.map( (i, enemy) => {
+            // get current enemy on loop 
+            let currentEnemy = $(enemy);
+            
+             // check for selected enemy
+             // in this case we only will remove the selected enemy
+             // but we need to make sure user only will be able to select 
+             // anothe enemy after he beats the previous selected one
+             if(i == enemyIndex){
+                 // remove selected enemy from view
+                 currentEnemy.addClass("hidden-element");
+             }
+            //  TODO: get array of #currentEnemyImgRow and display selected enemy
+         });
+         
+
         
     });
 
