fcb03553dec3020be802c18a8f3e72cec120efdb
Adliano Alves <adliano@me.com>
2019-03-21 00:51:42 -0700

    fix some colors and layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,29 +5,44 @@ Berkely Bootcamp
 JS file to hold all JQuery code used for RPG game
 */
 
-// attackerColumn
-//  attackerRow
-// enemiesImgsColumn
-// enemiesRow
-
 $(document).ready(function () {
-    // 1)  get all images belong to it parrent 
-    // 2)  to know witch on as select we can remove a special class name and get all the children using class name
-    // 3)  load all the other img to second row and change it to bg-danger
 
     // ******** Global Variables ******** \\
     // Flags (Booleans) used to control when
-    // user selects the Attacker and defender
+    // user selects the Attacker and Enemy
     var isAttackerSelected = false;
     var isEnemySelected = false;
+    // Attacker and Enemy attack rates
+    var userAttackRate = 0;
+    var enemyAttackRate = 0;
+    // Attacker and Enemy HP rate
+    var userHealth = 0;
+    var enemyHealth = 0;
     // ********************************* //
+
+    /******************************************************************************/ 
+    /* * * * * * * * * * * * * * * * * * rand() * * * * * * * * * * * * * * * * * */ 
+    /******************************************************************************/ 
+    // Function that generate random number                                          
+    // this will return a number beteween the provided range                         
+    // Math.random() return number between 0 (inclusive) and 1 (exclusive)           
+    // in this case, The maximum is inclusive and the minimum is inclusive
+    // it will be used to generate a random attack number           
+    function rand(min, max) {                                                        
+        min = Math.ceil(min);                                                        
+        max = Math.floor(max);                                                       
+        return Math.floor(Math.random() * (max - min + 1)) + min;                    
+    } 
 
     /////////////////////////////////// ONCLICK //////////////////////////////////////////
     //// onclick event added using class selector $(".setCharacter") for every image  ////
     //// this will be the first event listener to be called, inside we will use some  ////
     //// flags (Bollean) to void user click on selected character twice               ////
     //////////////////////////////////////////////////////////////////////////////////////
-    $(".setCharacter").on("click", function (event) {
+    $(".setCharacter").not(".attacker").on("click", function (event) {
+        
+        // Remove the onclick event from attacker
+        //$(this).off("click");
         // Check if user selected the Attacker
         if(!isAttackerSelected){
             // Get all elements except the one user selected
@@ -41,10 +56,19 @@ $(document).ready(function () {
             $("#enemiesRow").append(_siblings);
             //Display header
             $("#enemiesHeader").removeClass("invisible");
-            // 
+            // Change Attacker Header Text
             $("#attackerHeader").text("Your Attacker");
+            // Generate the Attack rate for user
+            userAttackRate = rand(5,25);
+            // Get Attacker Health
+            //userHealth = $(this).attr("data-health");
+            /////------- DEBUGGING ------\\\\\\
+            //console.log(userHealth);
             // Change the satus of the flag
             isAttackerSelected = !isAttackerSelected;
+            /////------- DEBUGGING ------\\\\\\
+            console.log(isAttackerSelected);
+            
         }
         // Check if user selected the Enemy
         else if(!isEnemySelected){
@@ -54,14 +78,18 @@ $(document).ready(function () {
             _enemy.children(".card").removeClass("bg-warning");
             // Add a Red background (Bootstrap)
             _enemy.children(".card").addClass("bg-danger");
-            // Add the selected enemy to enemyColumn
-            $("#enemyColumn").append(_enemy);
+            // Add the selected enemy to enemyRow
+            $("#enemyRow").append(_enemy);
             // Display Fight Header
             $("#fightHeader").removeClass("invisible");
             // Display Enemy Header
             $("#enemyHeader").removeClass("invisible");
             // Display Attack Button
             $("#btn-attack").removeClass("invisible");
+            // Generate enemy attack rate
+            enemyAttackRate = rand(5,25);
+            // Remove the onclick event from enemy
+            //_enemy.off("click");
             // Change the satus of the flag
             isEnemySelected = !isEnemySelected;
         } 
@@ -76,6 +104,7 @@ $(document).ready(function () {
     $("#btn-attack").on("click", function () {
         // debugging
         console.log("works!");
+        isEnemySelected = false;
     });
 
     ////////////////////////////////////////////
